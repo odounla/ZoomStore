44a6b2e455ee1dcc45a7dc3cdd08d0a71fb7f3cd
olivier <olivierdounla@hotmail.com>
2019-01-14 20:32:53 -0800

    zoomstore new look in progress

--- js/script.js
+++ js/script.js
@@ -1,12 +1,2 @@
-var main = function(){
-    $('.dropdown-toggle').click(function(){
-        $('.dropdrown-menu').toggle();
-    });
-};
 
-$(document).ready(main);
-
-$('#myTab a').on('click', function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
+  
